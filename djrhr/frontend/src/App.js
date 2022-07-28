5a4c3d5451f33680177f17fe275274a8898b3a33
Evan <evan@evansendra.com>
2022-07-28 22:54:53 +0100

    feat: proof of concept with hot reloading for django and react

---
+++ djrhr/frontend/src/App.js
@@ -0,0 +1,22 @@
+// App.js
+
+import React, { Component} from "react";
+
+
+class App extends Component {
+
+    constructor(){
+        super();
+    }
+
+    render(){
+        return(
+            <div>
+                This is the React index hotloaded...make a change.
+            </div>
+        )
+    }
+
+}
+
+export default App;
